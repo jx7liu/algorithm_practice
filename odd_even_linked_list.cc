b0ceebdd6143be82cd4734e2ff6cf04db267c372
Julia Liu <jingxliu0814@gmail.com>
2017-01-25 15:08:01 -0500

    Add files via upload
    grouping all odd after even nodes

---
+++ odd_even_linked_list.cc
@@ -0,0 +1,25 @@
+
+
+struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+     if (!head) return head;
+     
+     ListNode *odd = head;
+     ListNode *evenhead = head->next;
+     ListNode *even = head->next;
+     while (even && even->next){
+         odd->next = odd->next->next;
+         even->next = even->next->next;
+         odd = odd->next;
+         even = even->next;
+     }
+     odd->next = evenhead;
+     return head;
+    }
+    
+};
